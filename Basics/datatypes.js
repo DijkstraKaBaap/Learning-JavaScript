9d457158b9d16c288e624da37d1731769a2efbb8
DijkstraKaBaap <AdityanaiK064@gmail.com>
2024-09-01 18:52:31 +0000

    datatypes was done

---
+++ Basics/datatypes.js
@@ -0,0 +1,18 @@
+// string datatype
+name = "Dijkstra"
+
+// integer
+age = 500       // can be written upto 2^56 digits
+
+// boolean
+isAlive = true  // unfortunately
+
+// object
+const person = new Object();       // OR const person = new object();
+
+person.name = "Dijkstra"
+person.age = 30
+person.isAlive = false
+
+console.log(person)
+// this is simply anything that could be defined
