c1aa5f652758cc971594d532357c08ef210aa222
DijkstraKaBaap <AdityanaiK064@gmail.com>
2024-09-03 15:18:29 +0000

    control statements was done

---
+++ Basics/controlstatement.js
@@ -0,0 +1,10 @@
+var number = 1;
+if(number > 6){
+    console.log("The number is bigger than 6")
+}
+else if(number = 0) {
+    console.log("The number is 0")
+}
+else{
+    console.log("The number is less than 6")
+}
