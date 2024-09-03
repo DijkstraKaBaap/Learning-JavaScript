0604ab4f1e4980870ca459c997b4f8e526954239
DijkstraKaBaap <AdityanaiK064@gmail.com>
2024-09-03 15:57:14 +0000

    learnt switch case

--- Basics/controlstatement.js
+++ Basics/controlstatement.js
@@ -15,3 +15,33 @@ else{
 
 const anotherNo = 4
 console.log((anotherNo > 5) ? "This no. is greater than 5" : "This no. is less than 5")
+
+/*----------- Switch Case ---------*/
+// choice type control statement
+let day = 5;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thrusday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+        break;
+}
