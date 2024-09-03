ce0f738f3c8106ae0c857d01f587dfd6f4a4099b
DijkstraKaBaap <AdityanaiK064@gmail.com>
2024-09-03 15:35:00 +0000

    ..

---
+++ Basics/typeconversion.js
@@ -0,0 +1,44 @@
+// number typeof conversion
+let age = 30;
+console.log(typeof(age))
+
+// string
+let username = "Dijkstra123";
+console.log(typeof(username))
+
+// boolean
+let isAlive = true;
+console.log(typeof(isAlive))
+
+// null
+let illness = null;
+console.log(typeof(illness))
+
+// undefined
+let gf = undefined;
+console.log(typeof(gf))
+
+// Type conversion is done here
+let ageConvert = String(age)
+console.log(typeof(ageConvert))
+console.log(ageConvert)
+
+let usernameConvert = Number(username)
+let convertBool = Boolean(username)
+console.log(typeof(usernameConvert))
+console.log(typeof(convertBool))
+console.log(usernameConvert)
+console.log(convertBool)
+
+let isAliveConvert = Number(isAlive)
+console.log(typeof(isAliveConvert))
+console.log(isAliveConvert)
+
+let illnessConvert = Number(illness)
+console.log(typeof(illnessConvert))
+console.log(illnessConvert)
+
+let gfConvert = Number(gf)
+console.log(typeof(gfConvert))
+console.log(gfConvert)
+
