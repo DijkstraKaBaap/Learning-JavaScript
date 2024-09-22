0541026aee64aae476c6f4eebabc24d03497cbd1
DijkstraKaBaap <AdityanaiK064@gmail.com>
2024-09-22 10:59:41 +0000

    accessing object property and symbol dtype

--- Basics/Objects.js
+++ Basics/Objects.js
@@ -1,9 +1,12 @@
 // Objects in JavaScript
+const mysym = Symbol("night")
+
 let car = {
     brand : "Toyota",
     name : "Corolla",
     make : 2022,
     owner : "Mr. Dijkstra",
+    mysym : Symbol("Day"),
     drive : function(){
         console.log("The car is driving right now!")
     },
@@ -11,12 +14,17 @@ let car = {
         return this.brand + " " + this.name;
     }
 };
-console.log(car);
-console.log(car["make"]);
+
+console.log(car.owner);
+console.log(car["make"]);                         // more prefered method
+let action = car.drive
+car["car"]()                                    // does not output from function as property not considered a string
+car.drive
 
 // functions inside the objects are called methods
-var x = car;            // x IS car.
-x.drive();
+var x = car;                                      // x IS car.
 console.log(x.badgeToPrint());
+console.log(x.mysym)
+console.log(mysym)
 
 // object properties
